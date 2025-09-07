240d9f4ba89a3eb03d121b4fd818c700596d874a
Michael Conrad <m.conrad.202@gmail.com>
2025-09-07 01:21:34 -0400

    Step 06 - 03 - Exercise - Adding Pacman Game
    Introduce `PacmanGame` with movement controls and integrate into `AppGamingBasic`
    
    - Add `PacmanGame` class implementing `GamingConsole` interface
    - Update `AppGamingBasic` to demonstrate `PacmanGame` integration

--- src/main/java/com/michael_conrad/learn_spring_framework/AppGamingBasic.java
+++ src/main/java/com/michael_conrad/learn_spring_framework/AppGamingBasic.java
@@ -2,12 +2,14 @@ package com.michael_conrad.learn_spring_framework;
 
 import com.michael_conrad.learn_spring_framework.game.GameRunner;
 import com.michael_conrad.learn_spring_framework.game.MarioGame;
+import com.michael_conrad.learn_spring_framework.game.PacmanGame;
 import com.michael_conrad.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasic {
     public static void main(String[] args) {
         var game = new MarioGame();
         var superContraGame = new SuperContraGame();
+        var pacmanGame = new PacmanGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
