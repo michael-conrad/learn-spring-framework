5f914d7d5dbf79f7298b57573e1118fd184189a5
Michael Conrad <m.conrad.202@gmail.com>
2025-09-11 00:09:28 -0400

    Restructure to be multiple independent projects to match coursework

---
+++ learn-spring-framework-02/README.md
@@ -0,0 +1,5 @@
+# learn-spring-framework
+
+Course: [Master Spring Boot 3 &amp; Spring Framework 6 with Java
+](https://www.udemy.com/share/107zyk3@ybzVIwt9JJnJp_U4ZitjqJvTc813nKF8teYA4JNpAbjCCiX5_xxcc6a8b7QHktXVkg==/)
+
---
+++ learn-spring-framework-02/src/main/java/com/michael_conrad/learn_spring_framework/game/GameRunner.java
@@ -0,0 +1,17 @@
+package com.michael_conrad.learn_spring_framework.game;
+
+public class GameRunner {
+    private final GamingConsole game;
+
+    public GameRunner(GamingConsole game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running game: " + game.getClass().getSimpleName());
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
